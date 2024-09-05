f8675467c14bad7ce422bb5dd08b2c817e6c75d9
AadarshSoni123 <aadarsh.soni@intelliatech.com>
2024-09-05 16:14:54 +0530

    recent commit

---
+++ Task5.java
@@ -0,0 +1,38 @@
+package LogicBuilding;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.Optional;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+
+public class Task5 {
+
+	public static void main(String[] args) {
+		String str = "rtcode";
+		char[] ch = str.toCharArray();
+		
+		Map<Character, Long> in = str.chars()
+								 .mapToObj(i -> (char)i)
+								 .collect(Collectors.groupingBy(Function.identity(), 
+										 LinkedHashMap::new, Collectors.counting()));
+	
+		Optional<Character> firstNonRepeat = in.entrySet().stream().filter( (e) -> e.getValue() ==1)
+												.map(e -> e.getKey()).findFirst();
+		
+		Optional<Character> firstRepeating = in.entrySet().stream().filter( (e) -> e.getValue() > 1)
+												.map(e -> e.getKey()).findFirst();
+		 
+		if(firstNonRepeat.isEmpty()) 
+			System.out.println("Every element is repeating");
+		else if(firstNonRepeat.isPresent()) {
+			System.out.println("first non-repeating : " + firstNonRepeat.get());
+		}
+		
+		if(firstRepeating.isPresent())
+			System.out.println("First repeating : " + firstRepeating.get());
+		else if(firstRepeating.isEmpty())
+			System.out.println("Every element occuring once so none repeating");
+		
+	}
+}
